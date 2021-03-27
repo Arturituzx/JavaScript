3f7731c10f1105ead6362ecb0f5a37cf638c5722
Moises Falcon <mfalcon@csticorp.biz>
2021-03-27 16:18:00 -0500

    dirst commit

---
+++ script.js
@@ -0,0 +1,64 @@
+/*let numero = -5.500;
+let palabra = 'Hola Mundo';
+let respuesta = false;
+const PI = 3.14;
+numero = 100;
+palabra = 'Soy gay';
+respuesta = false;
+console.log(numero);
+console.log(palabra);
+console.log(respuesta);
+console.log(PI);*/
+
+
+
+/*Operadores Mate*/
+/*let a = 5;
+let b = 10;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(b/a);
+console.log(a%b);
+let usuario = 'otaku';
+console.log('Hola '+ usuario);*/
+
+/* Operadores Incrementeo y Decremento*/
+/*let a = 5;*/
+/* Suma*/
+/*console.log(a);
+console.log(a++);
+console.log(a);
+console.log(++a);
+/*console.log(a);*/
+
+
+/*Resta*/
+/*console.log(a--);
+console.log(a);
+console.log(--a);
+console.log(a);*/
+
+/*Operadores Asignación*/
+/*let a = 15; 
+a +=3;
+console.log(a);
+a -= 3;
+console.log(a);
+a *= 3;
+console.log(a);
+a /= 3;
+console.log(a);
+a %= 3;
+console.log(a);
+*/
+
+
+
+
+
+
+
+
+
+
