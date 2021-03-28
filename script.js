161928c5a6301316c9df1f449ca9c9d18be1a147
Moises Falcon <mfalcon@csticorp.biz>
2021-03-27 21:16:44 -0500

    primer dia de avance

--- script.js
+++ script.js
@@ -54,6 +54,127 @@ console.log(a);
 */
 
 
+/* String*/
+/*let cadena = 'Hola Putas ';*/
+/*let cadena2 = '     adfadfa asdfads f    '*/
+/*console.log(cadena.length);
+console.log(cadena.toUpperCase());
+console.log(cadena);
+console.log(cadena.toLowerCase());
+console.log(cadena.indexOf('Putas'));*/
+/*console.log(cadena.replace('Putas','Amo a las'));*/
+/*console.log(cadena.substring(0,5));
+console.log(cadena.slice(0,-3));*/
+/*console.log(cadena2.trim());*/
+/*console.log(cadena.startsWith('utas',6));*/
+/*console.log(cadena.includes('Putas',5));*/
+/* console.log(cadena.repeat('5'))*/
+
+/*let nombre = 'Aaron';
+let apeliido = 'Palomino';
+let edad = 18;
+console.log('Hola ' + nombre + ' ' + apeliido + '. tienes ' + (edad+1) + ' anos.');
+console.log(`Hola ${nombre} ${apeliido}. Tienes ${edad+10} anos.`);*/
+
+
+
+
+
+/*  Numeros Math*/
+
+
+/*console.log(Math.E);
+console.log(Math.PI);
+
+
+let num = 5.9;
+let den = 10;
+
+console.log(Math.abs(num));
+console.log(Math.ceil(num));
+console.log(Math.floor(num));
+console.log(Math.pow(2,5));
+console.log(Math.round(Math.random()*100));*/
+
+/* Condicionales */
+
+
+
+
+
+
+
+
+
+
+
+/*ordenar numeros de 3*/
+
+
+
+
+
+
+/*const numbers = document.getElementById('numbers');
+const result = document.getElementById('result');
+
+let a = prompt('Introduzca el primer número');
+let b = prompt('Introduzca el segundo número');
+let c = prompt('Introduzca el tercer número');
+
+numbers.textContent = `El número de anos que tines son ${a} ${b} ${c}`;
+
+if(a>=b && a>=c){
+    if(b>c){
+        result.textContent = `El orden es: ${a}, ${b}, ${c}`
+    }else{
+        result.textContent = `El orden es: ${a}, ${c}, ${b}`
+    }
+}else if(b>a && b>c){
+    if(a>c){
+        result.textContent = `El orden es: ${b}, ${a}, ${c}`
+    }else{
+        result.textContent = `El orden es: ${b}, ${c}, ${a}`
+    }
+}else if(c>=a && c>=b){
+    if(a>b){
+        result.textContent = `El orden es: ${c}, ${a}, ${b}`
+    }else{
+        result.textContent = `El orden es: ${c}, ${b}, ${a}`
+    }
+}
+
+
+/*Condicional switch*/
+
+
+/*let num = 4;
+switch(num){
+    case 1 : 
+        console.log(`${num} tiene el valor 1`); break;
+    case 2 : 
+        console.log(`${num} tiene el valor 2`); break;
+    
+    default: console.log(`${num} no vale ni 1 ni 2`)
+}
+
+switch(num){
+    case 1:
+    case 3:
+    case 5:
+        console.log(`${num} es impar`); break;
+    case 2:
+    case 4:
+        console.log(`${num} es par`);
+}*/
+
+
+   /*let num = prompt('ingresa num');
+num = parseInt(num)
+   if(num % 2 == 0) console.log(`${num} es par `); else console.log(`${num} es impar`);*/
+(num % 2 == 0) ? console.log(`${num} es par`) : console.log(`${num} es impar`);
+
+
 
 
 
